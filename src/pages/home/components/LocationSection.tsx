40f37841b690d7ebf783f0a2dc50e0fa8a8868d7
grabbedbyalienspayments-eng <grabbedbyalienspayments@gmail.com>
2026-01-07 17:00:06 +0200

    Update Google Maps embed URL in LocationSection
    Updated Google Maps embed URL for LocationSection.

--- src/pages/home/components/LocationSection.tsx
+++ src/pages/home/components/LocationSection.tsx
@@ -34,7 +34,7 @@ export default function LocationSection() {
           >
             <div className="h-96 rounded-xl overflow-hidden shadow-lg">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2848.8!2d26.0975!3d44.4268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDTCsDI1JzM2LjUiTiAyNsKwMDUnNTEuMCJF!5e0!3m2!1sen!2sro!4v1234567890"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2849.5648891234!2d26.10250831563842!3d44.42680007910234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDTCsDI1JzM2LjUiTiAyNsKwMDYnMDkuMCJF!5e0!3m2!1sro!2sro!4v1234567890123"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
